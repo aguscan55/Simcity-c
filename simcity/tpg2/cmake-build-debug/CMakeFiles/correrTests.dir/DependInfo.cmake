3361bed0111726861f7ac59398ee6013d1f7dc00
Canteros <acanteros@dc.uba.ar>
2023-06-13 13:57:37 -0300

    agrego simcity

---
+++ simcity/tpg2/cmake-build-debug/CMakeFiles/correrTests.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/src/Mapa.cpp" "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/cmake-build-debug/CMakeFiles/correrTests.dir/src/Mapa.cpp.o"
+  "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/src/Servidor.cpp" "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/cmake-build-debug/CMakeFiles/correrTests.dir/src/Servidor.cpp.o"
+  "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/src/SimCity.cpp" "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/cmake-build-debug/CMakeFiles/correrTests.dir/src/SimCity.cpp.o"
+  "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/src/aed2_Servidor.cpp" "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/cmake-build-debug/CMakeFiles/correrTests.dir/src/aed2_Servidor.cpp.o"
+  "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/tests/servidor-tests.cpp" "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/cmake-build-debug/CMakeFiles/correrTests.dir/tests/servidor-tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/cmake-build-debug/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/mnt/c/Users/caner/Desktop/tp-miko/algo2-2022c1-grupal-master/tpg2/cmake-build-debug/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ simcity/tpg2/cmake-build-debug/CMakeFiles/correrTests.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/correrTests.dir/src/Mapa.cpp.o"
+  "CMakeFiles/correrTests.dir/src/Servidor.cpp.o"
+  "CMakeFiles/correrTests.dir/src/SimCity.cpp.o"
+  "CMakeFiles/correrTests.dir/src/aed2_Servidor.cpp.o"
+  "CMakeFiles/correrTests.dir/tests/servidor-tests.cpp.o"
+  "correrTests"
+  "correrTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/correrTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
